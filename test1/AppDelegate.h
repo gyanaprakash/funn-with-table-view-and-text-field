2056d9d47abdefca3b4e35c89efbadd5793441d3
Bsetecip10 <bsetecip10@Nirmals-Mac-mini.local>
2014-10-17 11:40:38 +0530

    Initial Commit

---
+++ test1/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  test1
+//
+//  Created by Bsetecip10 on 17/10/14.
+//  Copyright (c) 2014 Bsetecip10. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
