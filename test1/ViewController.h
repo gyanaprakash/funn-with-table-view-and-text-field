5416196187caf7a08a892b7a121f8f5d045f7fe1
gyana prakash gouda <gyanaprakashgouda471@gmail.com>
2014-10-20 10:57:15 +0530

    for beginner
    crate text field according to text field value.

--- test1/ViewController.h
+++ test1/ViewController.h
@@ -10,6 +10,9 @@
 
 @interface ViewController : UIViewController
 
+@property (weak, nonatomic) IBOutlet UITextField *textfield;
+
+- (IBAction)buttonclicked:(id)sender;
 
 @end
 
--- test1/ViewController.m
+++ test1/ViewController.m
@@ -8,13 +8,17 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController ()<UIAlertViewDelegate>
 
 @end
 
 @implementation ViewController
-
-- (void)viewDidLoad {
+@synthesize textfield;
+- (void)viewDidLoad
+{
+    textfield.userInteractionEnabled=true;
+    [textfield setKeyboardAppearance:UIKeyboardAppearanceLight];
+    
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
 }
@@ -23,5 +27,49 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+//- (BOOL)textFieldShouldBeginEditing:(UITextField *)textField
+//{
+//    [textfield setKeyboardAppearance:UIKeyboardAppearanceLight];
+//    
+//}
+- (IBAction)resignkeyboard:(id)sender
+{
+    [self.view endEditing:YES];
+}
 
+- (void)textFieldDidEndEditing:(UITextField *)textField
+{
+    NSLog(@" did end editing :");
+    [textfield resignFirstResponder];
+        
+}
+//button clike method
+- (IBAction)buttonclicked:(id)sender
+{
+    
+    if (textfield.text.length==0)
+    {
+        NSLog(@"text filed empty");
+        UIAlertView *alert=[[UIAlertView alloc]initWithTitle:@"Warning !" message:@"Enter the value" delegate:self cancelButtonTitle:@"OK" otherButtonTitles:nil, nil];
+        [alert show];
+    }
+    else
+    {
+        CGFloat y=150;
+        int i;
+        
+        for (i=0; i<[textfield.text intValue]; i++)
+        {
+            UILabel *lable=[[UILabel alloc]initWithFrame:CGRectMake(40, y, 100, 40)];
+            
+            // increase the y value according to loop
+            y=y+50;
+            
+            // pass the interger value to the string
+            [lable setText:[NSString stringWithFormat:@"%d",i+1]];
+            [self.view addSubview:lable];
+        }
+        
+    }
+}
 @end
